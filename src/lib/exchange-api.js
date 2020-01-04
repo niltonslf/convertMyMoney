62d276551aee55ea1340f5d8addc0f11d632ea46
Nilton Lopes <niltonrck@gmail.com>
2020-01-04 19:18:54 -0300

    Adiciondo mais testes, adicionado cobertura de testes

--- src/lib/exchange-api.js
+++ src/lib/exchange-api.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
 const apiUrl = 'http://economia.awesomeapi.com.br/USD-BRL/1?format=json'
 
-const getExchangeRateAPI = axios.get(apiUrl)
+const getExchangeRateAPI = url => axios.get(url)
 const extractExchangeRate = res => res.data[0].high
+
 const getExchangeRate = async () => {
   try {
-    const res = await getExchangeRateAPI
+    const res = await getExchangeRateAPI(apiUrl)
     const exchangeRate = extractExchangeRate(res)
     return exchangeRate
   } catch (error) {
--- src/lib/exchange-api.test.js
+++ src/lib/exchange-api.test.js
@@ -1,7 +1,51 @@
-const { getExchangeRate } = require('./exchange-api')
+const {
+  getExchangeRateAPI,
+  extractExchangeRate,
+  getExchangeRate
+} = require('./exchange-api')
+const axios = require('axios')
 
-test('Exchage rate returned a value', async () => {
+jest.mock('axios')
+
+test('getExchangeRateAPI ', () => {
+  const res = {
+    data: [
+      {
+        high: 1.59
+      }
+    ]
+  }
+
+  axios.get.mockResolvedValue(res)
+
+  getExchangeRateAPI('url').then(response => {
+    expect(response).toEqual(res)
+    expect(axios.get.mock.calls[0][0]).toBe('url')
+  })
+})
+
+test('extractExchangeRate', () => {
+  const exchangeRate = extractExchangeRate({
+    data: [
+      {
+        high: 1.59
+      }
+    ]
+  })
+
+  expect(exchangeRate).toBe(1.59)
+})
+
+test('getExchangeRate', async () => {
+  const res = {
+    data: [
+      {
+        high: 1.59
+      }
+    ]
+  }
+  axios.get.mockResolvedValue(res)
+
   const price = await getExchangeRate()
-
-  expect(price).toBeGreaterThan(0)
+  expect(price).toBe(1.59)
 })
