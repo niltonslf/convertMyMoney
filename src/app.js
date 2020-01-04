ebe0c8f88ead92ea391ab9982b9250f3411d96f1
Nilton Lopes <niltonrck@gmail.com>
2020-01-03 21:23:48 -0300

    Pega port de um env

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-const express = require("express")
-const path = require("path")
+const express = require('express')
+const path = require('path')
 const app = express()
 
-const routes = require("./routes")
+const routes = require('./routes')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(routes)
 
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.listen(3000, () => {
+const port = proccess.env.PORT
+
+app.listen(port, () => {
   console.log(`Server started on port`)
 })
